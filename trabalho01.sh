c0ad849065ba7a283cf35c2e5c578eea83ec5f7f
Lucas Brito <lucassantanabrito@icloud.com>
2019-03-28 15:20:56 -0300

    Foi adicionado as funções de editar e remover.

--- trabalho01.sh
+++ trabalho01.sh
@@ -22,7 +22,16 @@ adicionar() {
         echo "${NOME}, ${SOBRENOME}, ${EMAIL}, ${TELEFONE}" 
         echo "Contato adicionado com sucesso!" 
     else
-        echo "Contato já exite!"
+        echo "Contato já exite, deseja atualizar o contato? S/N "
+        read OPT
+        if [[ ${OPT} = "S" ]]
+        then
+            sed -i "/${NOME}/d" bd.txt
+            echo "${NOME}, ${SOBRENOME}, ${EMAIL}, ${TELEFONE}" >> bd.txt 
+            echo "Contato atualizado com sucesso!"
+        else
+            echo ""
+        fi 
     fi
     echo "-----------------------------------------"
 }
@@ -41,7 +50,54 @@ pesquisar() {
     echo "PESQUISAR CONTATO"
     echo "Digite o nome do contato a ser pesquisado: "
     read NOME_PESQUISADO
-    grep -i ${NOME_PESQUISADO} bd.txt
+    VERIFICAR_NOME = `grep -i ${NOME_PESQUISADO} bd.txt`
+    if [[ ${VERIFICAR_NOME} == "" ]]
+    then
+        echo "Contato não encontrado!" 
+    else
+        echo "${VERIFICAR_NOME}"
+    fi
+    echo "-----------------------------------------"
+}
+
+editar() {
+    clear
+    echo "-----------------------------------------"
+    echo "PESQUISAR CONTATO"
+    echo "Digite o nome do contato a ser editado: "
+    read NOME_EDITADO
+    VERIFICAR_NOME = `grep -i ${NOME_PESQUISADO} bd.txt`
+    if [[ ${VERIFICAR_NOME} == "" ]]
+    then
+        echo "Contato não encontrado! Deseja adicionar o contato? S/N "
+        read OPT
+        if [[ ${OPT} = "S" ]]
+        then
+            adicionar
+        else
+            echo ""
+        fi
+
+    else
+        echo "Sobrenome: "
+        read SOBRENOME
+        echo "Email: "
+        read EMAIL
+        echo "Telefone: "
+        read TELEFONE
+        sed -i "/${NOME_EDITADO}/d" bd.tx
+        echo "${NOME_EDITADO}, ${SOBRENOME}, ${EMAIL}, ${TELEFONE}" >> bd.txt
+    fi
+    echo "-----------------------------------------"
+}
+
+deletar() {
+    clear
+    echo "-----------------------------------------"
+    echo "Deletar CONTATO"
+    echo "Digite o nome do contato a ser deletado: "
+    read NOME_DELETADO
+    sed -i "/${NOME_DELETADO}/d" bd.txt
     echo "-----------------------------------------"
 }
 
@@ -78,12 +134,13 @@ done
         1) adicionar;;
 
         2) pesquisar ;;
-        3) 
-            clear who ;;
-        4) 
-            clear who ;;
-        5) 
-            exibir ;;
+
+        3) deletar ;;
+        
+        4) clear who ;;
+        
+        5) exibir ;;
+        
         6) 
             clear
             echo Adios Amigo
